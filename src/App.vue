95281bcd85ea0051fa9a61ea07234b455f805256
Oliwia Oremek <ooremek@gmail.com>
2024-08-04 21:07:01 +0200

    v1.0

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,36 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup lang="ts">
+import text from './api/mock/data/mock-data.json';
+import Marker from './utils/marker';
+import { onMounted } from 'vue';
+
+const marker = new Marker({
+  className: 'highlight',
+})
+
+onMounted(() => {
+  marker.init();
+ 
+})
+
+const select = marker.mark;
+const unselect = marker.unmark;
+
+const options = {
+  itemHeight: 40,
+}
+const wrapper = {
+  height: '90vh',
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <button @click="select">Mark</button>
+  <button @click="unselect ">Unmark</button>
+  <div id='text' data-selectable>
+    <p v-for="item in text" :key="item.id" v-html="item.text"></p>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
---
+++ src/utils/marker.js
@@ -0,0 +1,62 @@
+import { createHighlighter } from 'rangy-highlighter'
+import { createClassApplier } from 'rangy-classapplier'
+
+class Marker {
+  constructor(options = {}) {
+    this.settings = {
+      className: 'highlight',
+      handleMarkerClick: () => {},
+      ...options,
+    }
+
+    this.highlighter = createHighlighter();
+
+
+    const classApplier = createClassApplier(this.settings.className, {
+      ignoreWhiteSpace: true,
+      elementTagName: 'mark',
+      elementProperties: {
+        onclick: (event) => {
+          const highlight = this.highlighter.getHighlightForElement(
+            event.target
+          )
+          this.settings.handleMarkerClick(highlight)
+        },
+      },
+    })
+
+    this.highlighter.addClassApplier(classApplier)
+
+    this.mark = this.mark.bind(this)
+    this.unmark = this.unmark.bind(this)
+  }
+
+  init() {
+    const savedHighlights = window.localStorage.getItem('highlights');
+    if(savedHighlights) {
+        this.highlighter.deserialize(savedHighlights);
+    }
+  }
+
+
+  highlight() {
+    return this.highlighter.highlightSelection(this.settings.className)[0];
+  }
+
+  mark() {
+   this.highlight();
+   const markers = Array.from(document.getElementsByTagName('mark'));
+   const markersInnerHTML = markers.map((marker, index) => ({innerHTML: marker.innerHTML, id: index}));
+    window.localStorage.setItem('markersJSON', JSON.stringify(markersInnerHTML));
+   const serialized = this.highlighter.serialize();
+   window.localStorage.setItem('highlights', serialized);
+  }
+
+  unmark() {
+    this.highlighter.unhighlightSelection();
+    const serialized = this.highlighter.serialize();
+    window.localStorage.setItem('highlights', serialized);
+  }
+}
+
+export default Marker
